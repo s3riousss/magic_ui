cc24c367952cf2badeb7374f2dc8dc66c341f2c5
s3riousss <s3riousss@gmail.com>
2024-04-30 15:49:23 +0200

    fix_driver

--- conftest.py
+++ conftest.py
@@ -18,6 +18,7 @@ def driver():
     options = Options()
     options.add_argument('--headless')
     # options.add_argument('start-maximized')
+    options.add_argument('window-size=1920,1080')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--log-level=3')
